89740d6e9140af569e4e845a1e03e74f5173a305
lopezp <lopezp9492@gmail.com>
2019-07-22 21:04:00 -0700

    Added Vars and Functions to Header

--- MinesweeperPractice/Board.h
+++ MinesweeperPractice/Board.h
@@ -1,10 +1,40 @@
 #pragma once
+#include <iostream>
+#include <vector>
+#include <time.h>       /* time */
+
+
 class Board
 {
 
 
 public:
 	Board();
+
 	~Board();
+
+	void initializeSize(int size);
+	void fill(double fillGoal);
+
+	void displayMines();
+	void display();
+
+	bool clear(int row, int col);
+	bool check(int row, int col);
+
+private:
+
+	//Variables
+	int size_;
+	std::vector<std::vector<bool> > mine_locations_;
+	std::vector<std::vector<bool> > checked_locations_;
+	std::vector<std::vector<int> > display_board_;
+
+	void resetMineLocations(int size);
+	void resetCheckedLocations(int size);
+	void resetDisplayBoard(int size);
+
+	void displayHeader(int size);		//helper function
+	bool inRange(int start, int end);	//helper funcion
 };
 
